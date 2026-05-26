291b6a8b8f18f253a3352acc705eceeaf4a5bbd7
SachinTripathi56 <agentsachin007@gmail.com>
2026-05-26 11:33:07 +0530

    last1

--- app/main.py
+++ app/main.py
@@ -98,9 +98,9 @@ origins = list(set(settings.ALLOWED_ORIGINS + [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://ai-fitness-backend-6v9k.onrender.com"
-    "https://fit-ai-companion-71.vercel.app/"
-    "https://fit-ai-companion-71.lovable.app/"
+    "https://ai-fitness-backend-6v9k.onrender.com",
+    "https://fit-ai-companion-71.vercel.app/",
+    "https://fit-ai-companion-71.lovable.app/",
 ]))
 
 app.add_middleware(
